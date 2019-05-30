a0f1d583cda8eb98aca775d06b5fc36cc3696ac9
Phillip Ahereza <pahereza@gmail.com>
2019-05-30 08:52:47 +0300

    added resume

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import resume from "./files/resume.pdf";
 
 class App extends Component {
   render() {
     const style = {
       fontWeight: 600
+    };
+
+    const underlineStyle = {
+      textDecoration: 'underline'
     }
     return (
       <div className="App">
         <p>
-          Hi, I’m <strong>Phillip</strong>.<br/>
-            <strong style={style}>Software Engineer</strong> at <a href="http://codeforafrica.org">Code for Africa</a>.
-      </p>
+          Hi, I’m <strong>Phillip</strong>.<br />
+          <strong style={style}>Software Engineer</strong> at{" "}
+          <a href="http://codeforafrica.org">Code for Africa</a>.
+        </p>
 
-          <section className='social'>
+        <section className="social">
+          <p>Follow me on:</p>
 
-            <p>Follow me on:</p>
+          <ul>
+            <li>
+              <a
+                href="https://github.com/phillipahereza"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github{" "}
+                <span role="img" aria-label="smiley face">
+                  🐙
+                </span>
+              </a>
+            </li>
+          </ul>
 
-            <ul>
-            <li><a href="https://github.com/phillipahereza" target='_blank' rel="noopener noreferrer">Github <span role="img" aria-label="smiley face">🐙</span></a></li>
-            </ul>
+          <p>Download my <a href={resume} style={underlineStyle}>Resumé</a></p>
+        </section>
 
-          </section>
-
-          <footer>
-            <p>This website will be updated as I learn React <span role="img" aria-label="smiley face">☺️</span></p>
-          </footer>
-              
+        <footer>
+          <p>
+            This website will be updated as I learn React{" "}
+            <span role="img" aria-label="smiley face">
+              ☺️
+            </span>
+          </p>
+        </footer>
       </div>
     );
   }
